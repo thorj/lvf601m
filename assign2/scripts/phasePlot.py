2ed6c7a999840200016ec91e56cfa9606c1634bf
thorj <t.jonmundsson@gmail.com>
2022-01-22 12:11:09 -0800

    add solutions

--- assign2/scripts/phasePlot.py
+++ assign2/scripts/phasePlot.py
@@ -7,8 +7,11 @@ from matplotlib.colors import Normalize
 # Define differential equation system
 def f(X, a1, a2, b1, b2, thetaA1, thetaA2, thetaB1, thetaB2, k1, k2, n):
     x1, x2 = X
-    dx1 = a1 * (x1**n)/(thetaA1**n + x1**n) + b1 * (thetaB1**n)/(thetaB1**n + x2**n) - k1 * x1
-    dx2 = a2 * (x2**n)/(thetaA2**n + x2**n) + b2 * (thetaB2**n)/(thetaB2**n + x1**n) - k2 * x2
+    dx1 = a1 * (x1**n)/(thetaA1**n + x1**n) + \
+        b1 * (thetaB1**n)/(thetaB1**n + x2**n) - k1 * x1
+    
+    dx2 = a2 * (x2**n)/(thetaA2**n + x2**n) + \
+        b2 * (thetaB2**n)/(thetaB2**n + x1**n) - k2 * x2
     return [dx1, dx2]
 
 # Define grid 
@@ -36,7 +39,9 @@ for i in range(NI):
     for j in range(NJ):
         x = X1[i, j]
         y = X2[i, j]
-        yP = f([x, y], a1, a2, b1, b2, thetaA1, thetaA2, thetaB1, thetaB2, k1, k2, n)
+        yP = f([x, y], a1, a2, b1, b2,  
+                thetaA1, thetaA2, thetaB1, thetaB2,   
+                k1, k2, n)
         u[i, j] = yP[0]
         v[i, j] = yP[1]
 
@@ -47,4 +52,4 @@ Q = plt.quiver(X1, X2, u, v, color, units = 'x', pivot = 'tip')
 plt.title('Replica of figure 3D [Huang]')
 plt.xlabel('$x_1$ = GATA1')
 plt.ylabel('$x_2$ = PU.1')
-plt.savefig('img/phasePlot.png')
+plt.savefig('img/phasePlot.png', dpi = 300)
